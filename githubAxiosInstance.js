494c8256612d60f4e15ae4b1f759ed09e423b871
Taump <taump@yandex.ru>
2021-12-24 21:08:31 +0700

    fix search

--- githubAxiosInstance.js
+++ githubAxiosInstance.js
@@ -4,7 +4,7 @@ const conf = require('ocore/conf.js');
 module.exports = axios.create({
   baseURL: 'https://api.github.com',
   headers: {
-    Authorization: `token ${conf.github_token}`,
+    Authorization: `token ${conf.github_tokens[0]}`,
     "User-Agent": "CASCADING DONATION",
     "Content-Type": "application/json"
   }
