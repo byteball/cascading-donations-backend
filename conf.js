ff7fe81417ad5e40e6461b527e17e03dd1b8d531
tony <tonyofbyteball@users.noreply.github.com>
2021-12-24 22:14:40 +0300

    fixed mainnet url and AA

--- conf.js
+++ conf.js
@@ -10,7 +10,7 @@ exports.testnet = process.env.testnet == "1";
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 
 exports.enableNotificationDiscord = true;
-exports.aa_address = process.env.testnet ? "IQDBISPZ555IZZNABYJSRWOF5ICFTQKW" : "U36LGRUXKSEIGGKAI5FF3GS5IHRNGD72";
+exports.aa_address = process.env.testnet ? "IQDBISPZ555IZZNABYJSRWOF5ICFTQKW" : "CHF2P27TMLTZ43KZ3BV5VSPT2ARIDCWC";
 
 exports.discord_channels = [process.env.discord_channel];
 exports.discord_token = process.env.discord_token;
@@ -23,7 +23,7 @@ exports.webserverPort = process.env.webserverPort;
 exports.github_tokens = process.env.github_tokens ? process.env.github_tokens.split(",") : [];
 
 exports.token_price_url = process.env.testnet ? "https://testnet.ostable.org/r/prices" : "https://referrals.ostable.org/prices";
-exports.bridge_url = process.env.testnet ? "https://testnet-bridge.counterstake.org/api/bridges" : "https://testnet.ostable.org/r/prices";
+exports.bridge_url = process.env.testnet ? "https://testnet-bridge.counterstake.org/api/bridges" : "https://counterstake.org/api/bridges";
 
 exports.watchedUsers = process.env.watchedUsers ? process.env.watchedUsers.split(/\s*,\s*/) : [];
 exports.watchedRepos = process.env.watchedRepos ? process.env.watchedRepos.split(/\s*,\s*/) : [];
